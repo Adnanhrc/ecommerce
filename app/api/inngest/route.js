1a580e34c307468f0a2c1e83fc49339e596ab170
Adnanhrc <mohammadnan88@gmail.com>
2025-06-01 10:45:52 +0530

    Fix: updated user deletion logic in route.js

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,5 +1,6 @@
 import { serve } from "inngest/next";
 import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
+import { User } from '@/models/user';
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
